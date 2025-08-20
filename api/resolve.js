6096358c9accd1ff508c23f2b1777bb01b04d130
StevenLiang026 <1754201193@qq.com>
2025-08-20 10:28:36 +0800

    实现真正的多服务器DNS查询，修复假多服务器查询问题

--- api/resolve.js
+++ api/resolve.js
@@ -1,4 +1,5 @@
 const dns = require('dns').promises;
+const { Resolver } = require('dns');
 
 // DNS服务器配置
 const DNS_SERVERS = {
@@ -11,42 +12,125 @@ const DNS_SERVERS = {
 // DNS记录类型
 const RECORD_TYPES = ['A', 'AAAA', 'MX', 'CNAME', 'TXT', 'NS', 'SOA', 'PTR'];
 
-async function executeDNSQuery(domain, recordType) {
+async function executeDNSQuery(domain, recordType, dnsServerIP = null) {
     try {
+        let resolver;
+        
+        // 如果指定了DNS服务器，创建专用解析器
+        if (dnsServerIP) {
+            resolver = new Resolver();
+            resolver.setServers([dnsServerIP]);
+        } else {
+            // 使用系统默认DNS
+            resolver = dns;
+        }
+
         let result;
         let formattedResult = '';
 
         switch (recordType.toUpperCase()) {
             case 'A':
-                result = await dns.resolve4(domain);
+                if (dnsServerIP) {
+                    result = await new Promise((resolve, reject) => {
+                        resolver.resolve4(domain, (err, addresses) => {
+                            if (err) reject(err);
+                            else resolve(addresses);
+                        });
+                    });
+                } else {
+                    result = await dns.resolve4(domain);
+                }
                 formattedResult = result.join('\n');
                 break;
             case 'AAAA':
-                result = await dns.resolve6(domain);
+                if (dnsServerIP) {
+                    result = await new Promise((resolve, reject) => {
+                        resolver.resolve6(domain, (err, addresses) => {
+                            if (err) reject(err);
+                            else resolve(addresses);
+                        });
+                    });
+                } else {
+                    result = await dns.resolve6(domain);
+                }
                 formattedResult = result.join('\n');
                 break;
             case 'MX':
-                result = await dns.resolveMx(domain);
+                if (dnsServerIP) {
+                    result = await new Promise((resolve, reject) => {
+                        resolver.resolveMx(domain, (err, addresses) => {
+                            if (err) reject(err);
+                            else resolve(addresses);
+                        });
+                    });
+                } else {
+                    result = await dns.resolveMx(domain);
+                }
                 formattedResult = result.map(mx => `${mx.priority} ${mx.exchange}`).join('\n');
                 break;
             case 'CNAME':
-                result = await dns.resolveCname(domain);
+                if (dnsServerIP) {
+                    result = await new Promise((resolve, reject) => {
+                        resolver.resolveCname(domain, (err, addresses) => {
+                            if (err) reject(err);
+                            else resolve(addresses);
+                        });
+                    });
+                } else {
+                    result = await dns.resolveCname(domain);
+                }
                 formattedResult = result.join('\n');
                 break;
             case 'TXT':
-                result = await dns.resolveTxt(domain);
+                if (dnsServerIP) {
+                    result = await new Promise((resolve, reject) => {
+                        resolver.resolveTxt(domain, (err, addresses) => {
+                            if (err) reject(err);
+                            else resolve(addresses);
+                        });
+                    });
+                } else {
+                    result = await dns.resolveTxt(domain);
+                }
                 formattedResult = result.map(txt => txt.join(' ')).join('\n');
                 break;
             case 'NS':
-                result = await dns.resolveNs(domain);
+                if (dnsServerIP) {
+                    result = await new Promise((resolve, reject) => {
+                        resolver.resolveNs(domain, (err, addresses) => {
+                            if (err) reject(err);
+                            else resolve(addresses);
+                        });
+                    });
+                } else {
+                    result = await dns.resolveNs(domain);
+                }
                 formattedResult = result.join('\n');
                 break;
             case 'SOA':
-                result = await dns.resolveSoa(domain);
+                if (dnsServerIP) {
+                    result = await new Promise((resolve, reject) => {
+                        resolver.resolveSoa(domain, (err, address) => {
+                            if (err) reject(err);
+                            else resolve(address);
+                        });
+                    });
+                } else {
+                    result = await dns.resolveSoa(domain);
+                }
                 formattedResult = `${result.nsname} ${result.hostmaster} ${result.serial} ${result.refresh} ${result.retry} ${result.expire} ${result.minttl}`;
                 break;
             case 'PTR':
-                result = await dns.resolvePtr(domain);
+                if (dnsServerIP) {
+                    result = await new Promise((resolve, reject) => {
+                        resolver.resolvePtr(domain, (err, addresses) => {
+                            if (err) reject(err);
+                            else resolve(addresses);
+                        });
+                    });
+                } else {
+                    result = await dns.resolvePtr(domain);
+                }
                 formattedResult = result.join('\n');
                 break;
             default:
@@ -110,6 +194,7 @@ module.exports = async (req, res) => {
 
         // 如果指定了DNS服务器，返回单服务器结果
         if (dnsServer && DNS_SERVERS[dnsServer]) {
+            const queryResult = await executeDNSQuery(domain, recordType.toUpperCase(), DNS_SERVERS[dnsServer]);
             res.json({
                 domain,
                 recordType: recordType.toUpperCase(),
@@ -120,15 +205,33 @@ module.exports = async (req, res) => {
             return;
         }
 
-        // 否则返回通用结果（模拟多服务器查询）
-        const results = Object.keys(DNS_SERVERS).map(serverName => ({
-            server: serverName,
-            serverIP: DNS_SERVERS[serverName],
-            domain,
-            recordType: recordType.toUpperCase(),
-            result: queryResult.result,
-            success: queryResult.success
-        }));
+        // 否则查询所有DNS服务器（真正的多服务器查询）
+        const promises = Object.entries(DNS_SERVERS).map(async ([serverName, serverIP]) => {
+            try {
+                const result = await executeDNSQuery(domain, recordType.toUpperCase(), serverIP);
+                return {
+                    server: serverName,
+                    serverIP: serverIP,
+                    domain,
+                    recordType: recordType.toUpperCase(),
+                    result: result.result,
+                    success: result.success,
+                    responseTime: Date.now() // 添加响应时间戳
+                };
+            } catch (error) {
+                return {
+                    server: serverName,
+                    serverIP: serverIP,
+                    domain,
+                    recordType: recordType.toUpperCase(),
+                    result: '查询失败',
+                    success: false,
+                    error: error.message
+                };
+            }
+        });
+
+        const results = await Promise.all(promises);
         
         res.json({
             domain,
